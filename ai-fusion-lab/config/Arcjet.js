39d3f492042018d08a37623809a29b2beb8dc47c
Sneha Singh <indsneha2002@gmail.com>
2025-11-10 10:46:49 +0530

    Added arcjet for rate limiting

---
+++ ai-fusion-lab/config/Arcjet.js
@@ -0,0 +1,16 @@
+import arcjet, { tokenBucket } from "@arcjet/next";
+
+export const aj = arcjet({
+  key: process.env.ARCJET_KEY, // Get your site key from https://app.arcjet.com
+  rules: [
+    // Create a token bucket rate limit. Other algorithms are supported.
+    tokenBucket({
+      mode: "LIVE",
+      characteristics: ["userId"],
+      refillRate: 5, // Refill 5 tokens per interval
+      interval: 10, // Refill every 10 seconds
+      capacity: 10, // Bucket capacity of 10 tokens
+    }),
+  ],
+});
+
